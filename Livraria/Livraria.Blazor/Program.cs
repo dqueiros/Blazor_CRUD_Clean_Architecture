e23c58a232666cc12d71fc04851219db7eddd88d
unknown <doqueiros@apoioprodesp.sp.gov.br>
2024-09-10 22:09:33 -0300

    Implementando os recursos do projeto Infrastructure e CrossCutting

--- Livraria/Livraria.Blazor/Program.cs
+++ Livraria/Livraria.Blazor/Program.cs
@@ -1,10 +1,14 @@
 using Livraria.Blazor.Components;
+using Livraria.CrossCutting.DependenciesApp;
+using Livraria.Infrastructure.Context;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
+
+builder.Services.AddInfrastructure(builder.Configuration);
 
 var app = builder.Build();
 
@@ -25,3 +29,10 @@ app.MapRazorComponents<App>()
     .AddInteractiveServerRenderMode();
 
 app.Run();
+
+static void CreateDatabase(WebApplication app)
+{
+    var serviceScope = app.Services.CreateScope();
+    var dataContext = serviceScope.ServiceProvider.GetService<ApplicationDbContext>();
+    dataContext?.Database.EnsureCreated();
+}
---
+++ Livraria/Livraria.Infrastructure/EntitiesConfiguration/LivroConfiguration.cs
@@ -0,0 +1,18 @@
+﻿using Livraria.Domain.Entities;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+
+namespace Livraria.Infrastructure.EntitiesConfiguration
+{
+    public class LivroConfiguration : IEntityTypeConfiguration<Livro>
+    {
+        public void Configure(EntityTypeBuilder<Livro> builder)
+        {
+            builder.HasKey(t => t.LivroId);
+            builder.Property(p => p.Titulo).HasMaxLength(150).IsRequired();
+            builder.Property(p => p.Autor).HasMaxLength(200).IsRequired();
+            builder.Property(p => p.Lancamento).IsRequired();
+            builder.Property(p => p.Capa).HasMaxLength(200);
+        }
+    }
+}
