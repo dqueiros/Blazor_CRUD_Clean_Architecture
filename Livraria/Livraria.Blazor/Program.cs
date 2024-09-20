291e30fc0898dd082179825705723b1532dec8d5
unknown <doqueiros@apoioprodesp.sp.gov.br>
2024-09-20 17:03:53 -0300

    Implementando os recursos do projeto Blazor:  Incluir e Exbir livros

--- Livraria/Livraria.Blazor/Program.cs
+++ Livraria/Livraria.Blazor/Program.cs
@@ -12,6 +12,8 @@ builder.Services.AddInfrastructure(builder.Configuration);
 
 var app = builder.Build();
 
+CreateDatabase(app);
+
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
